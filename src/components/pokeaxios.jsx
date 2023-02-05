4268974c1436c2176377bf36b17be0a94315a4d0
DaniPoka <danisschulz@gmail.com>
2023-02-05 02:23:30 -0300

    asdasd

--- src/components/pokeaxios.jsx
+++ src/components/pokeaxios.jsx
@@ -7,16 +7,6 @@ function PokeAxios() {
     const [nombres, setNombres] = useState([])
 
     const clickPokeAxios = () => {
-    //     fetch("https://pokeapi.co/api/v2/pokemon/?limit=807")
-    //         .then(response => {
-    //             return response.json();
-    //         }).then(response => {
-    //             setNombres(response.results.map((pokemon)=>pokemon.name));
-    //             console.log(response)
-    //         }).catch(err => {
-    //             console.log(err);
-
-    //         });
 
     axios.get("https://pokeapi.co/api/v2/pokemon/?limit=807")
             .then(response => {
